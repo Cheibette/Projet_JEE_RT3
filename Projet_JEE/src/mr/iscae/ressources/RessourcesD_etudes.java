93f059ded282b312ed24f9ee96879fbc68378f9a
abdellahicheibette <abdellahicheibette@gmail.com>
2019-01-12 22:35:25 +0000

    Notre 3eme commit, pour l'ajout d'un directeur d'etudes

--- Projet_JEE/src/mr/iscae/ressources/RessourcesD_etudes.java
+++ Projet_JEE/src/mr/iscae/ressources/RessourcesD_etudes.java
@@ -1,5 +1,29 @@
 package mr.iscae.ressources;
 
-public class RessourcesD_etudes {
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
+import mr.iscae.modeles.D_etudes;
+import mr.iscae.services.ServiceD_etudes;
+
+@Path("/D_etudesWS")
+@Produces(MediaType.APPLICATION_JSON)
+public class RessourcesD_etudes {
+	static ServiceD_etudes serviceD_etudes = new ServiceD_etudes();
+	@GET
+	@Path("/{IdD_etudes}")
+	public D_etudes getD_etudes(@PathParam("IdD_etudes") String idD_etudes){
+		return serviceD_etudes.getD_etudes(idD_etudes);
+	}
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public D_etudes addD_etudes(D_etudes d_etud){
+		return serviceD_etudes.ajoutD_etudes(d_etud);
+	}
+	
 }
--- Projet_JEE/src/mr/iscae/services/ServiceD_etudes.java
+++ Projet_JEE/src/mr/iscae/services/ServiceD_etudes.java
@@ -1,5 +1,38 @@
 package mr.iscae.services;
 
+import java.util.Map;
+
+import mr.iscae.bd.BaseDeDonnees;
+import mr.iscae.modeles.D_etudes;
+
 public class ServiceD_etudes {
+	private Map<String,D_etudes> d_etudes = BaseDeDonnees.getBdD_etudes();
+	
+	//Si on veut creer un directeur d'etudes par defaut :
+	public ServiceD_etudes(){
+		D_etudes D_etu = new D_etudes("MedMahmoud","Kedda",41570757);
+		//Les IDs des directeurs d'etudes sont sous forme I164XX 
+		D_etu.setID("I"+(16400+d_etudes.size()+1)+"");
+		d_etudes.put(D_etu.getID(), D_etu);
+	}
+	
+	//Fonction de GET, pour chercher un directeur d'etdudes
+	public D_etudes getD_etudes(String idD_etudes){
+		D_etudes D_etud=d_etudes.get(idD_etudes);
+		if(D_etud==null){
+			D_etudes d_etud = new D_etudes("null","null",0);
+			return d_etud;
+		}
+		return D_etud;
+	}
+	
+	//Fonction pour POST, ajout d'un directeur d'etdudes
+	
+	public D_etudes ajoutD_etudes(D_etudes d_etud){
+		d_etud.setID("I"+(16400+d_etudes.size()+1)+"");
+		d_etudes.put(d_etud.getID(), d_etud);
+		return d_etud;
+	}
+	
 
 }
